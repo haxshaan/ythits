3421363695c2ad463076788915c8f2cc5653c11b
Shantam Mathuria <shantam.m22@gmail.com>
2017-09-28 11:03:48 +0530

    Added chrome driver support with proxy

--- main.py
+++ main.py
@@ -15,44 +15,110 @@ def get_proxy(line_num):
                 return proxy
 
 
-def get_profile():
+class OpenBrowser:
 
-    p_host = str(get_proxy(line_num=current_line).split(':')[0])
-    p_port = int(get_proxy(line_num=current_line).split(':')[1])
+    def __init__(self, url):
+        self.url = url
 
-    f_profile = webdriver.FirefoxProfile()
+    def chrome_option(self):
 
-    f_profile.set_preference("browser.privatebrowsing.autostart", True)
-    f_profile.set_preference('network.proxy.type', 1)
-    f_profile.set_preference('network.proxy.http', p_host)
-    f_profile.set_preference('network.proxy.http_port', p_port)
-    f_profile.set_preference('network.proxy.ssl', p_host)
-    f_profile.set_preference('network.proxy.ssl_port', p_port)
-    f_profile.set_preference('http.response.timeout', 10)
+        proxy = get_proxy(line_num=current_line)
 
-    return f_profile
+        option = webdriver.ChromeOptions()
+
+        option.add_argument("--incognito")
+        option.add_argument("--proxy-server=http://%s" % proxy)
+
+    def open_chrome(self):
+
+        driver = webdriver.Chrome(executable_path='drivers/chromedriver.exe', chrome_options=self.chrome_option())
+
+        try:
+
+            driver.get(url=self.url)
+
+            try:
+                wait = WebDriverWait(driver=driver, timeout=15)
+
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME, "video-stream html5-main-video")))
+
+            except TimeoutException as t_e:
+                print "Page load time out!\nError: ", t_e
+                driver.quit()
+
+            except WebDriverException as w_e:
+                print "Proxy is probably dead!\nError: ", w_e
+                driver.quit()
+
+        except Exception, e:
+            print "Something went wrong while loading firefox!\nError: ", e
+
+        finally:
+            driver.quit()
+
+    def firefox_profile(self):
+
+        p_host = str(get_proxy(line_num=current_line).split(':')[0])
+        p_port = int(get_proxy(line_num=current_line).split(':')[1])
+
+        f_profile = webdriver.FirefoxProfile()
+
+        f_profile.set_preference("browser.privatebrowsing.autostart", True)
+        f_profile.set_preference('network.proxy.type', 1)
+        f_profile.set_preference('network.proxy.http', p_host)
+        f_profile.set_preference('network.proxy.http_port', p_port)
+        f_profile.set_preference('network.proxy.ssl', p_host)
+        f_profile.set_preference('network.proxy.ssl_port', p_port)
+        f_profile.set_preference('http.response.timeout', 10)
+
+        return f_profile
+
+    def open_firefox(self):
+
+        driver = webdriver.Firefox(executable_path=r'firefox_driver\geckodriver.exe', firefox_profile=self.firefox_profile())
+
+        try:
+
+            driver.get(url=self.url)
+
+            try:
+
+                wait = WebDriverWait(driver=driver, timeout=10)
+
+                wait.until(EC.presence_of_element_located((By.CLASS_NAME, "video-stream html5-main-video")))
+
+            except TimeoutException as t_e:
+                print "Something went wrong! Error: ", t_e
+                driver.quit()
+
+            except WebDriverException as w_e:
+                print "Proxy is probably dead! Error: ", w_e
+                driver.quit()
+
+        except Exception, e:
+            print "Something went wrong while loading firefox!\nError: ", e
+
+        finally:
+            driver.quit()
 
 
-def load_page():
-    try:
-        driver = webdriver.Firefox(firefox_profile=get_profile())
+def select_browser():
 
-        driver.get(my_link)
+    print "Select your browser. Press num key:"
+    while True:
+        try:
+            browser_name = int(raw_input("'1' Firefox\n'2' Chrome\nEnter you choice: "))
 
-        wait = WebDriverWait(driver=driver, timeout=10)
-
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "video-stream html5-main-video")))
-
-    except TimeoutException as t_e:
-        print "Something went wrong! Error: ", t_e
-        driver.quit()
-
-    except WebDriverException as w_e:
-        print "Proxy is probably dead! Error: ", w_e
-        driver.quit()
-
-    finally:
-        driver.quit()
+        except ValueError:
+            print "UhhOhh! Please enter only integer, Try again!"
+            continue
+        else:
+            break
+    if browser_name == 1:
+        print "You selected Firefox browser!\n"
+    else:
+        print "You selected Chrome browser!\n"
+    return browser_name
 
 
 def proxy_file_length():
@@ -61,18 +127,36 @@ def proxy_file_length():
         a = proxy_file.read()
         return a.count('\n')
 
-my_link = str(raw_input("Enter your link: "))
+my_link = str(raw_input("Enter your YouTube link: "))
 
 current_line = 1
 
-while not current_line == proxy_file_length() + 1:
+myYoutube = OpenBrowser(url=my_link)
 
-    print "Current Proxy: ", get_proxy(line_num=current_line)
+if select_browser() == 1:
+    while not current_line == proxy_file_length() + 1:
+        print "Entered loop.... test"
+        print "Current Proxy: ", get_proxy(line_num=current_line)
 
-    print "Now opening the url!"
+        print "Now opening the url!"
 
-    load_page()
+        myYoutube.open_firefox()
 
-    time.sleep(1)
+        time.sleep(1)
 
-    current_line += 1
+        current_line += 1
+
+else:
+
+    while not current_line == proxy_file_length() + 1:
+
+        print "Entered loop.... test"
+        print "Current Proxy: ", get_proxy(line_num=current_line)
+
+        print "Now opening the url!"
+
+        myYoutube.open_chrome()
+
+        time.sleep(1)
+
+        current_line += 1
